6f6ccf2f279b6154287c4ba5b6a7ae3475df1c3d
UBSITAlhambra <20124561@s.ubaguio.edu>
2025-11-25 02:36:43 +0800

    Updated database for vitals

--- finalproject.sql
+++ finalproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2025 at 07:26 PM
+-- Generation Time: Nov 24, 2025 at 07:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -95,6 +95,29 @@ INSERT INTO `studentrecord` (`ID`, `name`, `idNum`, `department`, `complaint`, `
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `student_vitals`
+--
+
+CREATE TABLE `student_vitals` (
+  `vitalID` int(11) NOT NULL,
+  `studentID` int(11) NOT NULL,
+  `vitalDate` date NOT NULL,
+  `temperature` decimal(4,1) DEFAULT NULL,
+  `bloodPressure` varchar(20) DEFAULT NULL,
+  `pulse` int(11) DEFAULT NULL,
+  `respiratoryRate` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_vitals`
+--
+
+INSERT INTO `student_vitals` (`vitalID`, `studentID`, `vitalDate`, `temperature`, `bloodPressure`, `pulse`, `respiratoryRate`) VALUES
+(2, 1, '2025-11-24', 999.9, '123/78', 123, 12);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `transaction`
 --
 
@@ -138,6 +161,13 @@ ALTER TABLE `studentrecord`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `student_vitals`
+--
+ALTER TABLE `student_vitals`
+  ADD PRIMARY KEY (`vitalID`),
+  ADD KEY `studentID` (`studentID`);
+
+--
 -- Indexes for table `transaction`
 --
 ALTER TABLE `transaction`
@@ -168,10 +198,26 @@ ALTER TABLE `studentrecord`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `student_vitals`
+--
+ALTER TABLE `student_vitals`
+  MODIFY `vitalID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
   MODIFY `transactionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `student_vitals`
+--
+ALTER TABLE `student_vitals`
+  ADD CONSTRAINT `student_vitals_ibfk_1` FOREIGN KEY (`studentID`) REFERENCES `studentrecord` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
